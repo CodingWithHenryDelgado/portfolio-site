1b6a9fcfd24af227e5a869d3687a9006cc5b8079
Henry_DelGlitch <CodingWHenry@gmail.com>
2023-03-10 16:07:05 -0500

    Menu has a z-index declared.

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function isInViewport(element) {
     return (
         rect.top >= 0 &&
         rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight * 2 || document.documentElement.clientHeight) &&
+        rect.bottom <= (window.innerHeight * 1.5 || document.documentElement.clientHeight) &&
         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
     );
 }
