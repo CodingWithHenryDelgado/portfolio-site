0891ee4ba42852abc07e91e73b456bb98a62daa7
Henry_DelGlitch <CodingWHenry@gmail.com>
2023-08-06 21:44:06 -0400

    Removed fade-in animation.

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
-const projectsCards = document.querySelectorAll('.projects-card');
+// const projectsCards = document.querySelectorAll('.projects-card');
 
-function isInViewport(element) {
-    const rect = element.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight * 1.5 || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-}
+// function isInViewport(element) {
+//     const rect = element.getBoundingClientRect();
+//     return (
+//         rect.top >= 0 &&
+//         rect.left >= 0 &&
+//         rect.bottom <= (window.innerHeight * 1.5 || document.documentElement.clientHeight) &&
+//         rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+//     );
+// }
 
-function handleScroll() {
-    projectsCards.forEach((card) => {
-        if (isInViewport(card)) {
-            card.classList.add('fade-in');
-        }
-    });
-}
+// function handleScroll() {
+//     projectsCards.forEach((card) => {
+//         if (isInViewport(card)) {
+//             card.classList.add('fade-in');
+//         }
+//     });
+// }
 
-window.addEventListener('scroll', handleScroll);
+// window.addEventListener('scroll', handleScroll);
